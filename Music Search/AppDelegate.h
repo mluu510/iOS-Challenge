31a6b25a3d9365862a8b01235921b06250fa6a6c
Minh Luu <mluu510@gmail.com>
2014-11-25 14:06:20 -0800

    Initial Commit

---
+++ Music Search/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Music Search
+//
+//  Created by Minh Luu on 11/25/14.
+//  Copyright (c) 2014 Minh Luu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
